13e54f67a76d0accaa16a909a243f4c9b0dbf874
joy-rout <jamejon15@gmail.com>
2022-12-19 19:55:38 +0530

    Navbar added

---
+++ src/Components/homePage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./homePage.css"
+const homePage = () => {
+
+  return (
+    <>
+      <div className='Homepage'>
+
+        
+        <div className='connect2Listener'>
+
+        </div>
+
+        <div className='seealltips'>
+
+        </div>
+
+        <div className='articles'>
+
+        </div>
+
+        <div className='books'>
+
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+export default homePage
